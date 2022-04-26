0fcb524f8e57ceb1b6ef5d9e93a191e3cd0c321c
SidharthaGundarapu <98143998+SidharthaGundarapu@users.noreply.github.com>
2022-04-26 17:22:48 -0500

    commit for authentication and validation

--- models/book.js
+++ models/book.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose")
 const bookSchema = mongoose.Schema({
-    Book_title: String,
-    genre: String,
-    cost: Number
+    Book_title: { 
+        type: String,
+        minLength: 10,
+        maxLength: 50, 
+        required: true,
+        trim: true
+    },
+    genre: { 
+        type: String, 
+        required: true
+    },
+    car_costcost: { 
+        type: Number, 
+        min:[5, 'Must be at least 5, got {VALUE}'], 
+        max:100
+    }
 })
 module.exports = mongoose.model("Book",bookSchema)
--- routes/books.js
+++ routes/books.js
@@ -8,15 +8,22 @@ router.get('/', function(req, res, next) {
   res.render('books', { title: 'Search Results' });
 });
 */
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 
 /* GET books */
-router.get('/', book_controller.book_view_all_Page );
+router.get('/', book_controller.book_view_all_Page);
 /* GET detail book page */
 router.get('/detail', book_controller.book_view_one_Page);
 /* GET create book page */
 router.get('/create', book_controller.book_create_Page);
 /* GET create update page */
-router.get('/update', book_controller.book_update_Page);
+router.get('/update',secured, book_controller.book_update_Page);
 /* GET delete book page */
 router.get('/delete', book_controller.book_delete_Page);
 
